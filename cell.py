72a01e0a5edd304d04b2b66f6f04ea3149ddab80
mateovg <17498931+mateovg@users.noreply.github.com>
2024-09-20 20:09:48 -0400

    Defaults for Cell. break_wall method

--- cell.py
+++ cell.py
@@ -1,22 +1,25 @@
+import random
 from graphics import Window, Line, Point
 from typing import TypeVar
 
 Cell = TypeVar("Cell", bound="Cell")
 
+
 class Cell:
     """
-    Class to represent a Cell of the maze. 
+    Class to represent a Cell of the maze.
     """
+
     def __init__(
         self,
-        pos: Point,
-        width: int,
-        height: int,
-        win: Window,
-        has_top_wall=True,
-        has_right_wall=True,
-        has_bottom_wall=True,
-        has_left_wall=True,
+        pos: Point = Point(0, 0),
+        width: int = 50,
+        height: int = 50,
+        win: Window = None,
+        has_top_wall: bool = True,
+        has_right_wall: bool = True,
+        has_bottom_wall: bool = True,
+        has_left_wall: bool = True,
     ) -> None:
         self._walls = [has_top_wall, has_right_wall, has_bottom_wall, has_left_wall]
         self._pos = pos
@@ -32,19 +35,39 @@ class Cell:
 
     def get_center(self) -> Point:
         half = self._size // 2
-        return Point(self._pos.x + half , self._pos.y + half)
+        return Point(self._pos.x + half, self._pos.y + half)
 
-    def draw_path(self, other: Cell, undo=False) -> None:
+    def draw_path(self, other: Cell, undo: bool = False) -> None:
+        if self._win is None:
+            return
+
         fill_color = "gray" if undo else "red"
         center_self = self.get_center()
         center_other = other.get_center()
         line = Line(center_self, center_other)
+
         self._win.draw_line(line, fill_color)
+
+    def break_wall(self, wall: int | None = None):
+        if wall is None:
+            wall = random.randint(0, 3)
+        # print(f"Breaking {self._pos}: Wall {wall}")
+        self._walls[wall] = False
 
     def draw(self, fill_color="black") -> None:
         for i in range(4):
             start, end = self._corners[i % 4], self._corners[(i + 1) % 4]
             line = Line(start, end)
-            wall = self._walls[i]
-            if wall:
+
+            if self._walls[i]:
+                # print(f"Cell: Printing: {self._pos}: Wall: {i}")
                 self._win.draw_line(line, fill_color)
+            else:
+                # print(f"Cell: Erasing: {self._pos}: Wall: {i}")
+                self._win.draw_line(line, "white")
+
+    def _get_walls(self) -> int:
+        """_summary_
+        Returns the number of walls in the cell
+        """
+        return sum(self._walls)
